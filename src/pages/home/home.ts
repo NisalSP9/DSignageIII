c7bb1beef098a4ee03c315aff25562f13521777f
nisalsp9 <nisalsp9@yahoo.com>
2018-08-10 17:02:06 +0530

    New modification done to read JSON and make screens.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,42 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  htmlToAdd:any;
+  myStyles = {
+    'margin-top': '0px',
+    'margin-bottom': '0px',
+    'height':'50%',
+    'border': 'dotted 1px'
+  };
+
+  myStyles2 = {
+
+    'margin': '0px',
+    // 'height':'150px',
+    'border': 'dotted 1px'
+  };
+  myStyles3 = {
+    'margin': '0px',
+    'height':'50%',
+    'border': 'dotted 1px'
+  };
+
+  public anArray:any=[
+    {'tag':'<img class="img1" src="../../assets/img/venom.jpg">','style':this.myStyles},
+    {'tag':'<marquee class="mq1">Venom is coming soon !!!</marquee>','style':this.myStyles2},
+    {'tag':'<video class="v2" autoplay loop src="../../assets/videos/VENOM%20.mp4"></video>','style':this.myStyles3}];
+
   constructor(public navCtrl: NavController) {
 
+    console.log('User created!');
+
+    // this.htmlToAdd = '<div class="two">two</
+
+    //this.htmlToAdd = '<img class="img1" src="../../assets/img/venom.jpg">'
+
   }
+
+
+
 
 }
